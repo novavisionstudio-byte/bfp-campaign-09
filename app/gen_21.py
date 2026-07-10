3a051b102ecf3d8c52df3df41665d16e442da246
campaign <campaign@thesis.local>
2026-07-10 17:24:51 +0100

    [028/risky] feat: large module gen_21 (light tests)

---
+++ app/gen_21.py
@@ -0,0 +1,160 @@
+"""Generated module 21 (campaign)."""
+
+
+def value_21(x):
+    """Return x scaled by 9 plus 2."""
+    return x * 9 + 2
+
+
+def helper_21_0(x):
+    return x + 0
+
+def helper_21_1(x):
+    return x + 1
+
+def helper_21_2(x):
+    return x + 2
+
+def helper_21_3(x):
+    return x + 3
+
+def helper_21_4(x):
+    return x + 4
+
+def helper_21_5(x):
+    return x + 5
+
+def helper_21_6(x):
+    return x + 6
+
+def helper_21_7(x):
+    return x + 7
+
+def helper_21_8(x):
+    return x + 8
+
+def helper_21_9(x):
+    return x + 9
+
+def helper_21_10(x):
+    return x + 10
+
+def helper_21_11(x):
+    return x + 11
+
+def helper_21_12(x):
+    return x + 12
+
+def helper_21_13(x):
+    return x + 13
+
+def helper_21_14(x):
+    return x + 14
+
+def helper_21_15(x):
+    return x + 15
+
+def helper_21_16(x):
+    return x + 16
+
+def helper_21_17(x):
+    return x + 17
+
+def helper_21_18(x):
+    return x + 18
+
+def helper_21_19(x):
+    return x + 19
+
+def helper_21_20(x):
+    return x + 20
+
+def helper_21_21(x):
+    return x + 21
+
+def helper_21_22(x):
+    return x + 22
+
+def helper_21_23(x):
+    return x + 23
+
+def helper_21_24(x):
+    return x + 24
+
+def helper_21_25(x):
+    return x + 25
+
+def helper_21_26(x):
+    return x + 26
+
+def helper_21_27(x):
+    return x + 27
+
+def helper_21_28(x):
+    return x + 28
+
+def helper_21_29(x):
+    return x + 29
+
+def helper_21_30(x):
+    return x + 30
+
+def helper_21_31(x):
+    return x + 31
+
+def helper_21_32(x):
+    return x + 32
+
+def helper_21_33(x):
+    return x + 33
+
+def helper_21_34(x):
+    return x + 34
+
+def helper_21_35(x):
+    return x + 35
+
+def helper_21_36(x):
+    return x + 36
+
+def helper_21_37(x):
+    return x + 37
+
+def helper_21_38(x):
+    return x + 38
+
+def helper_21_39(x):
+    return x + 39
+
+def helper_21_40(x):
+    return x + 40
+
+def helper_21_41(x):
+    return x + 41
+
+def helper_21_42(x):
+    return x + 42
+
+def helper_21_43(x):
+    return x + 43
+
+def helper_21_44(x):
+    return x + 44
+
+def helper_21_45(x):
+    return x + 45
+
+def helper_21_46(x):
+    return x + 46
+
+def helper_21_47(x):
+    return x + 47
+
+def helper_21_48(x):
+    return x + 48
+
+def helper_21_49(x):
+    return x + 49
+
+def helper_21_50(x):
+    return x + 50
