ea9abbe876b87fc255bbb05a24cc11a4ec328ff6
campaign <campaign@thesis.local>
2026-07-10 17:20:21 +0100

    [024/risky] feat: large module gen_17 (light tests)

---
+++ app/gen_17.py
@@ -0,0 +1,136 @@
+"""Generated module 17 (campaign)."""
+
+
+def value_17(x):
+    """Return x scaled by 7 plus 5."""
+    return x * 7 + 5
+
+
+def helper_17_0(x):
+    return x + 0
+
+def helper_17_1(x):
+    return x + 1
+
+def helper_17_2(x):
+    return x + 2
+
+def helper_17_3(x):
+    return x + 3
+
+def helper_17_4(x):
+    return x + 4
+
+def helper_17_5(x):
+    return x + 5
+
+def helper_17_6(x):
+    return x + 6
+
+def helper_17_7(x):
+    return x + 7
+
+def helper_17_8(x):
+    return x + 8
+
+def helper_17_9(x):
+    return x + 9
+
+def helper_17_10(x):
+    return x + 10
+
+def helper_17_11(x):
+    return x + 11
+
+def helper_17_12(x):
+    return x + 12
+
+def helper_17_13(x):
+    return x + 13
+
+def helper_17_14(x):
+    return x + 14
+
+def helper_17_15(x):
+    return x + 15
+
+def helper_17_16(x):
+    return x + 16
+
+def helper_17_17(x):
+    return x + 17
+
+def helper_17_18(x):
+    return x + 18
+
+def helper_17_19(x):
+    return x + 19
+
+def helper_17_20(x):
+    return x + 20
+
+def helper_17_21(x):
+    return x + 21
+
+def helper_17_22(x):
+    return x + 22
+
+def helper_17_23(x):
+    return x + 23
+
+def helper_17_24(x):
+    return x + 24
+
+def helper_17_25(x):
+    return x + 25
+
+def helper_17_26(x):
+    return x + 26
+
+def helper_17_27(x):
+    return x + 27
+
+def helper_17_28(x):
+    return x + 28
+
+def helper_17_29(x):
+    return x + 29
+
+def helper_17_30(x):
+    return x + 30
+
+def helper_17_31(x):
+    return x + 31
+
+def helper_17_32(x):
+    return x + 32
+
+def helper_17_33(x):
+    return x + 33
+
+def helper_17_34(x):
+    return x + 34
+
+def helper_17_35(x):
+    return x + 35
+
+def helper_17_36(x):
+    return x + 36
+
+def helper_17_37(x):
+    return x + 37
+
+def helper_17_38(x):
+    return x + 38
+
+def helper_17_39(x):
+    return x + 39
+
+def helper_17_40(x):
+    return x + 40
+
+def helper_17_41(x):
+    return x + 41
+
+def helper_17_42(x):
+    return x + 42
