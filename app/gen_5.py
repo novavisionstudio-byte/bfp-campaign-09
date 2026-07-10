e4cba0b66d618cd3e200433e94be397e4d359ee3
campaign <campaign@thesis.local>
2026-07-10 17:00:01 +0100

    [008/risky] feat: large module gen_5 (light tests)

---
+++ app/gen_5.py
@@ -0,0 +1,91 @@
+"""Generated module 5 (campaign)."""
+
+
+def value_5(x):
+    """Return x scaled by 5 plus 3."""
+    return x * 5 + 3
+
+
+def helper_5_0(x):
+    return x + 0
+
+def helper_5_1(x):
+    return x + 1
+
+def helper_5_2(x):
+    return x + 2
+
+def helper_5_3(x):
+    return x + 3
+
+def helper_5_4(x):
+    return x + 4
+
+def helper_5_5(x):
+    return x + 5
+
+def helper_5_6(x):
+    return x + 6
+
+def helper_5_7(x):
+    return x + 7
+
+def helper_5_8(x):
+    return x + 8
+
+def helper_5_9(x):
+    return x + 9
+
+def helper_5_10(x):
+    return x + 10
+
+def helper_5_11(x):
+    return x + 11
+
+def helper_5_12(x):
+    return x + 12
+
+def helper_5_13(x):
+    return x + 13
+
+def helper_5_14(x):
+    return x + 14
+
+def helper_5_15(x):
+    return x + 15
+
+def helper_5_16(x):
+    return x + 16
+
+def helper_5_17(x):
+    return x + 17
+
+def helper_5_18(x):
+    return x + 18
+
+def helper_5_19(x):
+    return x + 19
+
+def helper_5_20(x):
+    return x + 20
+
+def helper_5_21(x):
+    return x + 21
+
+def helper_5_22(x):
+    return x + 22
+
+def helper_5_23(x):
+    return x + 23
+
+def helper_5_24(x):
+    return x + 24
+
+def helper_5_25(x):
+    return x + 25
+
+def helper_5_26(x):
+    return x + 26
+
+def helper_5_27(x):
+    return x + 27
